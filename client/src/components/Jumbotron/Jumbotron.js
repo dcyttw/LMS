dbda7c9a09873072aba88203e28ebde8d6d5efe2
Nick Volkert <nick@nickvolkert.com>
2018-05-06 22:45:11 -0500

    adding components

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+  <Jumbotron>
+    <h1>Northwestern Coding Bootcamp</h1>
+  </Jumbotron>>
+);
+
+export default Jumbotron;
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Login.css";
+
+Const Login = props => (
+  <div>
+    <h1>Login Stuff goes here</h1>
+  </div>
+);
+
+export default Login;
