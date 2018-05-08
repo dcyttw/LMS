4615a6ecbb20e9fb6edfda3080f2f8a1c405013f
Nick Volkert <nick@nickvolkert.com>
2018-05-07 20:33:07 -0500

    adding pages and page components

---
+++ client/src/pages/Attendance/Attendance.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Navbar from "../components/Navbar";
+import Jumbotron from "../components/Jumbotron"
+import Footer from "../components/Footer";
+
+class Attendance extends Component {
+  // Setting our component's initial state
+  // state = {
+  //   books: [],
+  //   title: "",
+  //   author: "",
+  //   synopsis: ""
+  // };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <h1>Northwestern Coding Bootcamp</h1>
+            </Jumbotron>
+          </Col>
+          <Col size="md-6 sm-12">
+            //Auth0 Stuff
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Attendance;
---
+++ client/src/pages/Sessions/Sessions.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+class Sessions extends Component {
+  // Setting our component's initial state
+  // state = {
+  //   books: [],
+  //   title: "",
+  //   author: "",
+  //   synopsis: ""
+  // };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <h1>Northwestern Coding Bootcamp</h1>
+            </Jumbotron>
+          </Col>
+          <Col size="md-6 sm-12">
+            //Auth0 Stuff
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Sessions;
